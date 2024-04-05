53e5b0fe90c53e0125e9573af7bf7b747eedbb95
tonylapoche <terrade.antoine.pro@gmail.com>
2024-04-05 14:01:17 +0200

    Ajout des composants header et footer dans le composant principal

--- frontend/my-app/src/app/app.component.ts
+++ frontend/my-app/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MainComponent } from './core/layout/main/main.component';
+import { HeaderComponent } from './core/layout/header/header.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [MainComponent],
+  imports: [MainComponent, HeaderComponent, FooterComponent],
 })
 export class AppComponent {}
