02be2ee3c90954fae1601dab0548dbd50b9fa857
tonylapoche <terrade.antoine.pro@gmail.com>
2024-04-05 10:18:33 +0200

    Mise à jour du composant principal dans le frontend

--- frontend/my-app/src/app/app.component.ts
+++ frontend/my-app/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './views/public/home/home.component';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './core/layout/header/header.component';
+import { MainComponent } from './core/layout/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  imports: [MainComponent],
 })
 export class AppComponent {}
--- frontend/my-app/src/app/core/layout/main/main.component.ts
+++ frontend/my-app/src/app/core/layout/main/main.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrl: './main.component.css',
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
